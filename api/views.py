f3dd1114c421bfd60658471ed0efa4662dbde647
samekmat <samekmat@gmail.com>
2025-10-17 19:30:33 +0200

    feat(api): add API logic

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from articles.models import Article
+from.serializers import ArticleSerializer
+
+
+class ArticleListView(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.all()
+        source = self.request.GET.get('source')
+        if source is not None:
+            queryset = queryset.filter(source_domain=source)
+        return queryset
+
+class ArticleDetailView(generics.RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
